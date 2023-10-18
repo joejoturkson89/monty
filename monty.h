a21c0b0d4e98472c3434780454900c41256eb270
joejoturkson89 <turksonjoejo@gmail.com>
2023-10-18 05:24:43 -0700

    correction

--- monty.h
+++ monty.h
@@ -39,11 +39,6 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern int SQ;
-instruction_t *parse_line(char *line);
-void free_stack(stack_t *head);
-char *get_opcode(char *string);
-int is_int(char *str);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
@@ -51,16 +46,8 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
-void divide(stack_t **stack, unsigned int line_number);
-void multiply(stack_t **stack, unsigned int line_number);
-void moddy(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
-void rotl(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, unsigned int line_number);
-void stack(stack_t **stack, unsigned int line_number);
-void queue(stack_t **stack, unsigned int line_number);
 
+int is_digit(char *string);
+int is_number(char *str);
 
 #endif
